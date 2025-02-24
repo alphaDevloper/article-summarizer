3cb320567d4bbb464ebe789a9ce354f5b9eff944
alphaDevloper <abdullahdanyal1234@gmail.com>
2025-02-24 19:21:02 +0500

    color changed

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -2,13 +2,14 @@ const Hero = () => {
   return (
     <header className="w-full flex justify-center items-center flex-col">
       <nav className="flex justify-between items-center w-full mb-10 pt-3">
-        <h1 className="text-orange-400 font-bold text-3xl mx-9 my-4">
+        <h1 className="bg-linear-to-r/hsl from-indigo-500 to-teal-400 bg-clip-text text-transparent font-bold text-3xl mx-9 my-4">
           Summarize
         </h1>
       </nav>
       <h1 className=" mt-5 text-5xl font-extrabold leading-[1.15] text-black sm:text-6xl text-center">
         Summarize Articles with <br className="max-md:hidden" />{" "}
-        <span className="bg-gradient-to-r from-amber-500 via-orange-600 to-yellow-500 bg-clip-text text-transparent">
+        {/* bg-linear-to-r/hsl from-indigo-500 to-teal-400 */}
+        <span className="bg-linear-to-r/hsl from-indigo-500 to-teal-400 bg-clip-text text-transparent">
           OpenAI
         </span>
       </h1>
